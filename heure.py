bc16a2d5f0880e66e901ffd113a3ce166ca0ef25
ensai <thomas.soulie@eleve.ensai.fr>
2024-01-15 16:17:23 +0100

    suite

--- heure.py
+++ heure.py
@@ -1,5 +1,11 @@
 from datetime import datetime
+from dateutil import tz
 
 time = str(datetime.now())
+timeb = datetime.now()
 print(time[11:19])
-datetime.hour
+
+time_reunion = timeb.astimezone(tz.gettz('Indian/Reunion')).time()
+time_paris = timeb.astimezone(tz.gettz('Europe/Paris')).time()
+
+print(time_paris, time_reunion)
